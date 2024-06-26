2a2b1bb66c80c6ca258453caab21dbe91ca64ca2
jitendra5kumar <86180479+jitendra5kumar@users.noreply.github.com>
2024-06-26 19:38:38 +0530

    update contact

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { contactForm } from "../controller/ContactController.js"
+import { contactForm } from "../controller/contactController.js"
 const router = express.Router()
 
 router.post("/contact", contactForm)

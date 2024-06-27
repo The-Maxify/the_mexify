c4ece690b91caf9630cb948593972c90947d688d
jitendra5kumar <86180479+jitendra5kumar@users.noreply.github.com>
2024-06-27 13:10:41 +0530

    update about

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.json(__dirname, "./frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html")); // Corrected here
 });
+
 
 const port = process.env.PORT || 3000;
 app.use("/", contactRouter);
